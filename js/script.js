7b09a23832b8d95abb1933dd548a5ffd8c89b856
leanhpham <43395904+leanhpham@users.noreply.github.com>
2023-11-09 01:01:36 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 function calcul() {
   let total = document.getElementById('total').value;
   let vip = document.getElementById('vip').value;
-  let normal = document.getElementById('normal').value;
-  if (isNaN(total) || isNaN(vip) || isNaN(normal) || total <= 0 || vip <= 0 || normal <= 0) {
+  let total_guest = document.getElementById('total_guest').value;
+  if (isNaN(total) || isNaN(vip) || isNaN(total_guest) || total <= 0 || vip <= 0 || total_guest <= 0) {
     alert('Nhập số dương !');
   }
+  console.log(total_guest,vip);
+  if (parseInt(total_guest) < parseInt(vip)) {
+    alert('Tổng Số Khách phải lớn hơn Số Khách Lên Vip !');
+  }
   else {
-    vip_paid = Math.round(parseInt(total) / (parseInt(vip) + parseInt(normal)))*4;
+    vip_paid = Math.round(parseInt(total) / (parseInt(vip) + parseInt(total_guest - vip)))*4;
     document.getElementById('vip_paid').innerHTML = vip_paid.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});;
-    normal_paid = Math.round(parseInt(total) / (parseInt(vip) + parseInt(normal)));
+    normal_paid = Math.round(parseInt(total) / (parseInt(vip) + parseInt(total_guest - vip)));
     document.getElementById('normal_paid').innerHTML = normal_paid.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});;
-    total_paid = vip_paid*parseInt(vip) + normal_paid*parseInt(normal);
+    total_paid = vip_paid*parseInt(vip) + normal_paid*parseInt(total_guest-vip);
     document.getElementById('total_paid').innerHTML = total_paid.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});;
     event.preventDefault()
   }
@@ -19,7 +23,7 @@ function calcul() {
 function clearResult() {
     document.getElementById('total').value = 0;
     document.getElementById('vip').value = 0;
-    document.getElementById('normal').value = 0;
+    document.getElementById('total_guest').value = 0;
     document.getElementById('vip_paid').innerText = '';
     document.getElementById('normal_paid').innerText = '';
     document.getElementById('total_paid').innerText = '';
